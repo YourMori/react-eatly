c951d20aed968a17b9ee23143915c0be3348462d
YourMori <pa4ok_28@mail.ru>
2024-04-26 01:49:48 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<header className='header'>
+				<div className='container'>bgbfbgbgf</div>
+			</header>
+
+			<main className='main'></main>
+
+			<footer className='footer'></footer>
+		</>
+	);
+}
+
+export default App;
