f029891dcc695fdaf496103be93e4c6d86721aa3
YourMori <pa4ok_28@mail.ru>
2024-04-26 21:37:33 +0700

    created header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,38 +4,38 @@ const Header: React.FC = () => {
 	return (
 		<header className={styles.header}>
 			<div className='container'>
-				<div className='header__wrapper'>
+				<div className={styles.header__wrapper}>
 					<a className='header__logo'>
 						<img src='./src/assets/Logo.svg'></img>
 					</a>
-				</div>
-				<nav className='header__menu'>
-					<ul className='menu__list'>
-						<li className='menu__list--item'>
-							<a href='#' className='menu__link'>
-								Menu
-							</a>
-						</li>
-						<li className='menu__list--item'>
-							<a href='#' className='menu__link'>
-								Blog
-							</a>
-						</li>
-						<li className='menu__list--item'>
-							<a href='#' className='menu__link'>
-								Pricing
-							</a>
-						</li>
-						<li className='menu__list--item'>
-							<a href='#' className='menu__link'>
-								Contacts
-							</a>
-						</li>
-					</ul>
-				</nav>
-				<div className=''>
-					<button className='header__button header__button--login'></button>
-					<button className='header__button header__button--signup'></button>
+					<nav className={styles.header__menu}>
+						<ul className='menu__list'>
+							<li className='menu__list--item'>
+								<a href='#' className='menu__link'>
+									Menu
+								</a>
+							</li>
+							<li className='menu__list--item'>
+								<a href='#' className='menu__link'>
+									Blog
+								</a>
+							</li>
+							<li className='menu__list--item'>
+								<a href='#' className='menu__link'>
+									Pricing
+								</a>
+							</li>
+							<li className='menu__list--item'>
+								<a href='#' className='menu__link'>
+									Contacts
+								</a>
+							</li>
+						</ul>
+					</nav>
+					<div className={styles.header__button}>
+						<button className={styles.header__button_login}>Login</button>
+						<button className={styles.header__button_signup}>Sign up</button>
+					</div>
 				</div>
 			</div>
 		</header>
